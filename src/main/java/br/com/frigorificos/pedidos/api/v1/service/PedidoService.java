867e99872258bc2ad58cc6339b659d5a07e41990
matheusdpo <matheusoliveira1991@hotmail.com>
2025-02-26 21:18:03 -0300

    service para enfileirar pedido

---
+++ src/main/java/br/com/frigorificos/pedidos/api/v1/service/PedidoService.java
@@ -0,0 +1,27 @@
+package br.com.frigorificos.pedidos.api.v1.service;
+
+import br.com.frigorificos.pedidos.api.v1.dto.PainelDTO;
+import br.com.frigorificos.pedidos.api.v1.utils.LogUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private LogUtils logger;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    public PainelDTO enfileirarEmissoes(PainelDTO painelDTO) {
+        rabbitTemplate.convertAndSend(exchangeName, "", painelDTO);
+        logger.info("Pedido enfileirado: " + painelDTO.toString());
+        return painelDTO;
+    }
+}
